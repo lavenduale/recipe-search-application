82bf8091fc92ac36290fb3166a6b88f7b40a94bd
Yusong Du <yd3127@mun.ca>
2019-04-22 23:28:50 -0230

    package update

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,39 @@
 // node package to get access
 const path = require('path');
+const HtmlWebpackPlugin = require(`html-webpack-plugin`);
 
 module.exports = {
     // entry point object is where it starts
-    entry: './src/js/index.js',
+    entry: ['babel-polyfill', './src/js/index.js'],
     // output object is tell the webpack where to save the bundle file
     output: {
         // the path is absolute so it needs a built in node package to have access
-        path: path.resolve(__dirname, 'dist/js'), // __dirname is current absolute path; dist/js is the one we want to bundle with
-        filename: 'bundle.js'
+        path: path.resolve(__dirname, 'dist'), // __dirname is current absolute path; dist/js is the one we want to bundle with
+        filename: 'js/bundle.js'
     },
     // development mode simply builds our bundler without minifying our code in order to be as fast as possible
-    mode: 'development'
-}
+    //mode: 'development'
+
+    // for webpack devServer config
+    devServer: {
+        // for which webpack should serve, here is in distribution folder
+        contentBase: './dist'
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: './src/index.html'
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            }
+        ]
+    }
+};
